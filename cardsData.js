58386747f03a29da3ee047ba97c2b63458639fb7
juandisay <juandi.syafrin@gmail.com>
2020-11-30 09:39:53 +0700

    Create cardsData.js

---
+++ cardsData.js
@@ -0,0 +1,12 @@
+let cardsData = [{
+  id: 1,
+  image: 'https://source.unsplash.com/h-ACUrBngrw/1280x720',
+  avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+  user: {
+    name: 'juandisay',
+    handle: '@juandisay',
+    title: 'Lead Developer'
+  },
+  content: 'The apps using Django and VueJs'
+},
+]
