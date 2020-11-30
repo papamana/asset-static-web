c491e6134da80b98e2c39f836d9bd669ab831d6a
juandisay <juandi.syafrin@gmail.com>
2020-11-30 10:20:38 +0700

    Update cardsData.js

--- cardsData.js
+++ cardsData.js
@@ -3,10 +3,12 @@ let cardsData = [{
   image: 'https://source.unsplash.com/h-ACUrBngrw/1280x720',
   avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
   user: {
+    title: 'kasQui Mechine',
     name: 'juandisay',
     handle: '@juandisay',
-    title: 'Lead Developer'
+    role: 'Lead Developer'
+    
   },
-  content: 'The apps using Django and VueJs'
+  content: 'The application answers your complaints about managing transactions in your business. This application consists of Cashier, CRM, and Inventory.'
 },
 ]
